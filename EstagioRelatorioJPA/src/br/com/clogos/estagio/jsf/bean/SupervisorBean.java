0e1e6900fd400e935ad53cc4fbd1b6a43adc1e3c
Tiago Arruda <tiagoarruda.astec@hotmail.com.br>
2014-04-25 17:03:35 -0300

    Spring

---
+++ EstagioRelatorioJPA/src/br/com/clogos/estagio/jsf/bean/SupervisorBean.java
@@ -0,0 +1,10 @@
+package br.com.clogos.estagio.jsf.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class SupervisorBean {
+	
+}
---
+++ EstagioRelatorioJPA/src/br/com/clogos/estagio/jsf/facade/SupervisorFacade.java
@@ -0,0 +1,9 @@
+package br.com.clogos.estagio.jsf.facade;
+
+import java.io.Serializable;
+
+public class SupervisorFacade implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+
+}
