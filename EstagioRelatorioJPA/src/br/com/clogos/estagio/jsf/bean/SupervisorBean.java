ff950da23984a808d55b74776977443bf341e0e3
tiago <tiago@wbserver.clogos.com.br>
2014-05-06 00:15:40 -0300

    Cadastro Supervisor

--- EstagioRelatorioJPA/src/br/com/clogos/estagio/jsf/bean/SupervisorBean.java
+++ EstagioRelatorioJPA/src/br/com/clogos/estagio/jsf/bean/SupervisorBean.java
@@ -1,10 +1,70 @@
 package br.com.clogos.estagio.jsf.bean;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
+
+import org.apache.commons.io.IOUtils;
+import org.primefaces.model.UploadedFile;
+
+import br.com.clogos.estagio.jsf.facade.SupervisorFacade;
+import br.com.clogos.estagio.model.Supervisor;
 
 @ManagedBean
 @ViewScoped
-public class SupervisorBean {
+public class SupervisorBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private SupervisorFacade facade;
+	private UploadedFile uploadedFile;
+	private Supervisor supervisor;
+	private static final String DIRETORIO = "C:/";
+	
+	public SupervisorFacade getFacade() {
+		return facade == null ? new SupervisorFacade() : facade;
+	}
+	
+	public UploadedFile getUploadedFile() {
+		return uploadedFile;
+	}
+	
+	public void setUploadedFile(UploadedFile uploadedFile) {
+		this.uploadedFile = uploadedFile;
+	}
+	
+	public Supervisor getSupervisor() {
+		return supervisor == null ? new Supervisor() : supervisor;
+	}
+	
+	public void setSupervisor(Supervisor supervisor) {
+		this.supervisor = supervisor;
+	}
+	
+	public void save(ActionEvent event) {
+		try {
+			String nomeArquivo = new SimpleDateFormat("ddMMyyyyHHmmssSSS").format(new Date())+getSupervisor().getNome();
+			File file = new File(DIRETORIO+nomeArquivo+".png");
+			IOUtils.copyLarge(getUploadedFile().getInputstream(), new FileOutputStream(file));
+			
+			System.out.println(file.getName());
+		
+//			getArquivo().setCaminho(DIRETORIO);
+//			getArquivo().setTamanho((int) getUpload().getSize());
+//			getArquivo().setNome(file.getName());
+//			getFacade().save(getArquivo());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}                    
+	}
+	
 	
 }
--- EstagioRelatorioJPA/src/br/com/clogos/estagio/jsf/facade/SupervisorFacade.java
+++ EstagioRelatorioJPA/src/br/com/clogos/estagio/jsf/facade/SupervisorFacade.java
@@ -2,8 +2,21 @@ package br.com.clogos.estagio.jsf.facade;
 
 import java.io.Serializable;
 
+import br.com.clogos.estagio.model.Supervisor;
+
 public class SupervisorFacade implements Serializable {
 	private static final long serialVersionUID = 1L;
+	private Supervisor supervisor;
 	
-
+	public Supervisor getSupervisor() {
+		if(supervisor == null) {
+			supervisor = new Supervisor();
+		}
+		return supervisor;
+	}
+	public void setSupervisor(Supervisor supervisor) {
+		this.supervisor = supervisor;
+	}
+	
+	
 }
