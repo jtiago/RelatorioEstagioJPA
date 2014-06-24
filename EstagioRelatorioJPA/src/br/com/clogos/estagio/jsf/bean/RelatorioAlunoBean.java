be8561dc154450ec49fd2c275e3cb396218a9732
Tiago Arruda <tiagoarruda.astec@hotmail.com.br>
2014-06-24 11:35:31 -0300

    Impl - Sicoob

--- EstagioRelatorioJPA/src/br/com/clogos/estagio/jsf/bean/RelatorioAlunoBean.java
+++ EstagioRelatorioJPA/src/br/com/clogos/estagio/jsf/bean/RelatorioAlunoBean.java
@@ -4,15 +4,21 @@ import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+import javax.faces.event.ComponentSystemEvent;
+import javax.servlet.http.HttpSession;
 
 import br.com.clogos.estagio.jsf.facade.RelatorioAlunoFacade;
+import br.com.clogos.estagio.model.Aluno;
 
 @ManagedBean(name="relatorioAlunoBean")
 @ViewScoped
 public class RelatorioAlunoBean implements Serializable {
 	private static final long serialVersionUID = 6378902231545732309L;
 	private RelatorioAlunoFacade facade;
+	private Boolean renderedEnfermagem;
+	private Boolean renderedRadiologia;
 	
 	public RelatorioAlunoFacade getFacade() {
 		return facade == null ? facade = new RelatorioAlunoFacade() : facade;
@@ -26,19 +32,37 @@ public class RelatorioAlunoBean implements Serializable {
 		getFacade().limpar();
 	}
 	
-	public Boolean getMensagemEnf(String modulo) {
-		if(modulo.contains("II M") || modulo.contains("III M")) {
-			return true;
+	public void defineRendered(ComponentSystemEvent event) {
+		FacesContext context = FacesContext.getCurrentInstance();
+		HttpSession httpSession = (HttpSession) context.getExternalContext().getSession(false); 
+		Aluno aluno = (Aluno) httpSession.getAttribute("usuarioLogado");
+		
+		if(aluno.getModulo().contains("II M") || aluno.getModulo().contains("III M")) {
+			renderedEnfermagem = true;
 		} else {
-			return false;
+			renderedEnfermagem = false;
+		}
+		
+		if((aluno.getModulo().contains("II E III")) && (aluno.getTurmaT().getNomeCurso().contains("radiologia") || aluno.getTurmaT().getNomeCurso().contains("RADIOLOGIA"))) {
+			renderedRadiologia =  true;
+		} else {
+			renderedRadiologia =  false;
 		}
 	}
 	
-	public Boolean getMensagemRad(String modulo, String curso) {
-		if(modulo.contains("II e III") && (curso.contains("radiologia")) || curso.contains("RADIOLOGIA")) {
-			return true;
-		} else {
-			return false;
-		}
+	public Boolean getRenderedEnfermagem() {
+		return renderedEnfermagem;
+	}
+
+	public void setRenderedEnfermagem(Boolean renderedEnfermagem) {
+		this.renderedEnfermagem = renderedEnfermagem;
+	}
+
+	public Boolean getRenderedRadiologia() {
+		return renderedRadiologia;
+	}
+
+	public void setRenderedRadiologia(Boolean renderedRadiologia) {
+		this.renderedRadiologia = renderedRadiologia;
 	}
 }
