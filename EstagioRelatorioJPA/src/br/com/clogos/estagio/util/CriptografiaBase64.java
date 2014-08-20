c6c11b5891caa00ba24f1479061607629033bc49
jtiago <jtiago@suporte>
2014-08-20 00:13:50 -0300

    impl - casa

--- EstagioRelatorioJPA/src/br/com/clogos/estagio/util/CriptografiaBase64.java
+++ EstagioRelatorioJPA/src/br/com/clogos/estagio/util/CriptografiaBase64.java
@@ -12,7 +12,7 @@ public class CriptografiaBase64 {
 	}
 	
 	 public static void main(String[] args) {
-		//System.out.println(CriptografiaBase64.encrypt("22"));
-		 System.out.println(CriptografiaBase64.decrypt("NTU="));
+		System.out.println(CriptografiaBase64.encrypt("22"));
+		 //System.out.println(CriptografiaBase64.decrypt("NTU="));
 	}
 }
