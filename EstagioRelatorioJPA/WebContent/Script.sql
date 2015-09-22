0beebcb9858fb5b90244fac3672fd393c0b4d286
Developer <Developer@192.168.25.32>
2015-09-22 00:49:57 -0300

    impl

--- EstagioRelatorioJPA/WebContent/Script.sql
+++ EstagioRelatorioJPA/WebContent/Script.sql
@@ -11,7 +11,7 @@ INSERT INTO perfil
 (nomeperfil,cadastroAluno,cadastroCampo,cadastroSupervisor,cadastroTurma,liberarRelatorio,relatorioAluno,relatorioAdmin,revisaoRelatorio,cadastroUsuario,relatorioEnviado)
 VALUES ('SUPERVISOR',0,0,0,0,0,0,1,0,0,0);
 
-INSERT INTO SEMESTRE VALUES (DEFAULT, "2014.2"), (DEFAULT, "2015.1");
+INSERT INTO SEMESTRE VALUES (DEFAULT, "2015.2");
 
 -- Criação de Usuário
 INSERT INTO usuario (cpfusuario,nomeusuario,senhausuario,fkperfil) VALUES ('22222222222', 'Arruda', 'MjI=', 1)
