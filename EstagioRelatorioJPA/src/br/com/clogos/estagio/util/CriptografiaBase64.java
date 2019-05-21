4b444c082625deb401e55764af86fb0b162bdeff
Suporte <Suporte@192.168.25.106>
2019-05-20 23:47:43 -0300

    altera as datas relacionadas abaixo: LOGOSABER 21/01/2019 à 31/01/2019 LOGOSAÚDE 09/05/2019 à 10/05/2019

--- EstagioRelatorioJPA/src/br/com/clogos/estagio/util/CriptografiaBase64.java
+++ EstagioRelatorioJPA/src/br/com/clogos/estagio/util/CriptografiaBase64.java
@@ -13,6 +13,6 @@ public class CriptografiaBase64 {
 	
 	 public static void main(String[] args) {
 		//System.out.println(CriptografiaBase64.encrypt("22"));
-		System.out.println(CriptografiaBase64.decrypt("MTIz"));
+		System.out.println(CriptografiaBase64.decrypt("MTIyMDIzNDY="));
 	}
 }
