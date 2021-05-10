36b8a47b1533ad07f3ef275c0c2bb818a284a727
tiago.santos <tiago.santos@sicoob.com.br>
2021-05-10 19:49:21 -0300

    Atualização datas logos saber e saúde na ficha de avaliação.

--- EstagioRelatorioJPA/src/br/com/clogos/estagio/jsf/bean/SupervisorBean.java
+++ EstagioRelatorioJPA/src/br/com/clogos/estagio/jsf/bean/SupervisorBean.java
@@ -1,7 +1,5 @@
 package br.com.clogos.estagio.jsf.bean;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -10,7 +8,6 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.event.ActionEvent;
 
-import org.apache.commons.io.IOUtils;
 import org.primefaces.model.UploadedFile;
 
 import br.com.clogos.estagio.jsf.facade.SupervisorFacade;
@@ -32,20 +29,17 @@ public class SupervisorBean implements Serializable {
 	}
 	
 	public void save() {
-		try {
-			String nomeArquivo = new SimpleDateFormat("ddMMyyyyHHmmssSSS").format(new Date())+".jpg";
-			File file = new File(DIRETORIO+nomeArquivo+".jpg");
-			IOUtils.copyLarge(getFile().getInputstream(), new FileOutputStream(file));
 			
-			ImagemAssinatura assinatura = new ImagemAssinatura();
-			assinatura.setNome(nomeArquivo);
-			assinatura.setCaminho(DIRETORIO);
-			getFacade().save(assinatura);
-			file = null;
+		String nomeArquivo = new SimpleDateFormat("ddMMyyyyHHmmssSSS").format(new Date());
+		// File file = new File(DIRETORIO+nomeArquivo+".jpg");
+		// IOUtils.copyLarge(getFile().getInputstream(), new FileOutputStream(file));
+		
+		ImagemAssinatura assinatura = new ImagemAssinatura();
+		assinatura.setNome(nomeArquivo);
+		assinatura.setCaminho(DIRETORIO);
+		getFacade().save(assinatura);
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}                    
+		                   
 	}
 	
 	public void remove(ActionEvent event) {
